1141c083bd7a7f3846f8f5bc0d31330c755f651c
OrtwIn <braunbertil123@outlook.de>
2020-04-15 21:47:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -10,15 +10,6 @@ class Quad:
   def __repr__(self):
     return "X: " + str(self.x) + " Y: " + str(self.y) + " W: " + str(self.w) + " H: " + str(self.h) 
 
-  def compare(self, o):
-    if self.y != o.y:   return self.y < o.y
-    if self.x != o.x:   return self.x < o.x
-    if self.w != o.w:   return self.w > o.w
-    return self.h >= o.h
-
-  def inside(self, o):
-    return o.y <= self.y and o.x <= self.x and o.x + o.w >= self.x + self.w and o.y + o.h >= self.y + self.h
-
 def debug(o):
   OKBLUE = '\033[94m'
   ENDC = '\033[0m'
@@ -30,17 +21,16 @@ def debug(o):
     for i, mer in enumerate(merg):
       symb = '1' if mer else '0'
       if i >= q.x and i < q.x + q.w and j >= q.y and j < q.y + q.h:
-        print (OKBLUE + symb + ENDC, end='')
-      else:
-        print (symb, end='')
+	    symb = OKBLUE + symb + ENDC
+      print (symb, end='')
     print ()
 
 """ 
 
 looped through left to right, then top to bottom
 
-	  -> -> -> -> -> -> -> -|
-							|
+      -> -> -> -> -> -> -> -|
+                            |
 |<--------------------------v
 |
 v->   -> -> -> -> ...
@@ -60,6 +50,8 @@ X: 2 Y: 2 W: 2 H: 1
 X: 6 Y: 2 W: 1 H: 1
 
 """
+
+
 world = [
   [1, 1, 1, 1, 2, 2, 1],
   [1, 1, 1, 1, 2, 2, 1],
@@ -69,42 +61,57 @@ world = [
 n = len(world)
 m = len(world[0])
 
-o = [] # list of output quads
-merged = [[False for _ in range(m)] for _ in range(n)] # if tile was visited -> True and therefore doesnt have to be checked again and meshed again
+# list of output quads
+o = [] 
+# if tile was visited -> True and therefore doesnt have to be checked again and meshed again
+merged = [[False for _ in range(m)] for _ in range(n)]
 
-for y in range(n): # loop through map
+# loop through map
+for y in range(n):
   x = 0
   while x < m:
     
-    if merged[y][x]: # if allready merged -> continue to look at next tile
+	# if allready merged -> continue to look at next tile
+    if merged[y][x]:
       x += 1
       continue
 
-    t = world[y][x] # block at world coordinates
-    w = h = 1 # width and height of quad
+	# block at world coordinates
+    t = world[y][x]
+	# width and height of quad
+    w = h = 1
 
-    while x + w < m and t == world[y][x + w] and not merged[y][x + w]: 
 		# x + w < m -> while in width bounds
 		# t == world[y][x + w] -> while its the same kind of tile
 		# not merged[y][x + w] -> not allready looked at
-      w += 1 # increase width and continue expanding to the right
+    while x + w < m and t == world[y][x + w] and not merged[y][x + w]: 
+	  # increase width and continue expanding to the right
+      w += 1
 
-    while y + h < n: # while in height bounds
+	# while in height bounds
+    while y + h < n:
       good = True
-      for i in range(w): # loop through row
-        if t != world[y + h][x + i]: # if the tile is different -> cant expand downwards
+	  # loop through row
+      for i in range(w):
+		# if the tile is different -> cant expand downwards
+        if t != world[y + h][x + i]:
           good = False
           break
       if good:
-        h += 1 # whole row of the width of the row above matches -> expand height
+		# the whole row of the width of the row above matches -> expand height
+        h += 1
       else:
         break
     
     for j in range(h):
       for i in range(w):
-        merged[y + j][x + i] = True # set all of the new matched tiles to True to not further look at
+		# set all of the new matched tiles to True to not further look at
+        merged[y + j][x + i] = True
     
-    o.append(Quad(x, y, w, h, t)) # append to output
-    x += w # expand by with and continue looking there for new quads
+	# append a new Quad to the output
+    o.append(Quad(x, y, w, h, t))
+	# expand by with and continue looking there for new quads
+    x += w
 
-    debug(o) # dump debug info to console
+	# dump debug info to console
+    debug(o)
